322ea8eecf67026c2a213bab0ec79e37d611b701
HiiPower05 <fordarkseid13@gmail.com>
2026-08-16 21:16:09 +0200

    Add Bootstrap navigation and foundation pages

---
+++ APPR.coreproject/Pages/Donate/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APPR.coreproject.Pages.Donate
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
