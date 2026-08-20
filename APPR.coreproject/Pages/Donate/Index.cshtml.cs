fce55451ca205873a04d7cf3130adc52f7e9c981
HiiPower05 <fordarkseid13@gmail.com>
2026-08-20 12:47:00 +0200

    Add donation form and database saving

--- APPR.coreproject/Pages/Donate/Index.cshtml.cs
+++ APPR.coreproject/Pages/Donate/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using APPR.coreproject.Data;
+using APPR.coreproject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,32 @@ namespace APPR.coreproject.Pages.Donate
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+            private readonly ApplicationDbContext _context;
+            // Gives access to database.
+            public IndexModel(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            [BindProperty]
+            public Donation Donation { get; set; } = new Donation();
+            public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Donation.DonationDate = DateTime.Now;
+
+            _context.Donations.Add(Donation);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
